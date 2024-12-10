b461d8d46754bae3f7554bb9bc1f843f35ac191f
MuhammedMidlajk <midlajmuhamd@gmial.com>
2024-12-10 11:01:15 +0530

    JAVASCRIPT-FUNCTION

---
+++ script.js
@@ -0,0 +1,228 @@
+
+// function add(a,b){
+
+//     return a+b
+// }
+// console.log(add(5,1));
+
+
+
+
+
+// // function
+
+// function function_name(){
+
+//     body;
+// }
+// function_name();
+
+
+
+// function dark(a, b){
+//  sum= a+b;
+//  multi = a*b;
+//     console.log(sum +"\n"+ multi);
+//     console.log(multi);
+// }
+// dark(5, 7);
+
+
+
+
+// var a=10;
+// function dark(){
+//     var  s=10
+//     var sums = s+5
+//     return sums
+
+// }
+// console.log(dark());
+
+
+
+
+// function squar(x){
+
+//     return x*x
+
+// }
+// console.log(squar(5) );   
+
+
+
+// function dark(C){
+
+//     return(C*(9/5) + 32.)
+// }
+// var celsius=dark(2)
+// console.log(celsius);
+
+
+
+// function b(hight,width){
+
+//     return(hight*width)
+// }
+// var Rectangle=b(5,2)
+
+// console.log(Rectangle);
+
+
+
+// function d(number) {
+//     let fac = 1;
+//     for (let i = 1; i <= number; i++) {
+//         fac = fac * i
+
+//     }
+//     return fac
+// }
+
+// let Factorial = d(5)
+
+// console.log(Factorial) 
+
+
+
+
+
+// function g(number){
+
+//     console.log(Math.random());
+// }
+// g();
+
+
+
+
+
+
+
+
+
+
+
+// function a(){
+
+//     console.log("hello world");
+
+// }
+// a();
+
+
+
+
+// function square(a){
+
+//     return(a*a);
+
+// }
+// let result=square(5)
+// console.log(result)
+
+
+
+// function change(Celsius){
+
+//     return Celsius * 9/5 + 32
+
+// }
+//  let dark=change(5)
+//  console.log(dark)
+
+
+
+
+// function sum(length,width){
+
+//     return length*width
+// }
+// console.log(sum(10,5))
+
+
+
+
+
+// function factorial(a){
+
+//     let j=1
+//     for(i=1;i<=a;i++){
+//         j=i*j
+
+//     }
+//     // console.log(j)
+
+//     return j
+// }
+// // factorial(5)
+
+// let dark=factorial(5)
+// console.log(dark)
+
+// console.log(factorial(5))
+
+
+
+
+// console.log(Math.round(Math.random()*100))
+
+
+
+
+// function random(){
+
+//     console.log(Math.random())
+// }
+// random()
+
+
+
+
+
+// learn
+
+// function add(dot){
+
+//     var a=dot+dot
+//     return a
+
+// }
+
+// var b=add(1)
+// console.log(b)
+
+
+
+
+// function palindrome(a){
+
+//           f  
+
+// }
+
+// var n=2
+// function square (num){
+//     var ans = num*num;
+//     return ans;
+// }
+// var square2 = square(n);
+// var square4 = square(4);
+
+// console.log();
+
+
+
+// function x() {
+//     var a = 7
+//     function y() {
+     
+//         console.log(a);
+
+//     }
+//     var a=100
+//     return y;
+// }
+// var z = x();
+// console.log(z);
+// z()
